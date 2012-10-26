d7827d045a13102a377f7439429e716b70ac1072
Andreas Atteneder <andreas.atteneder@gmail.com>
2012-10-27 01:56:11 +0200

    color scheme, introduced codekit (sass, jslint)

--- js/main.js
+++ js/main.js
@@ -1,40 +1,76 @@
-$(document).ready(function() {
-  $("#distance").change(updateDistance);
+(function(){
+"use strict";
 
-  $("#timeHour").change(onTimeUpdate);    
-  $("#timeMin").change(onTimeUpdate);
-  $("#timeSec").change(onTimeUpdate);
-  
-  $("#paceMin").change(onPaceUpdate);
-  $("#paceSec").change(onPaceUpdate);
-});
-
-undefinedPace = true;
+var undefinedPace = true;
 
 function getTime() {
-  var time = parseInt($("#timeHour").val()) * 3600;
-  if(isNaN(time)) time=0;
-  time += parseInt($("#timeMin").val()) * 60;
-  time += parseInt($("#timeSec").val());
+  var time = parseInt($("#timeHour").val(),10) * 3600;
+  if(isNaN(time)) {
+    time=0;
+  }
+  time += parseInt($("#timeMin").val(),10) * 60;
+  time += parseInt($("#timeSec").val(),10);
   return time;
 }
 
 function getPace() {
-  var pace = parseInt($("#paceMin").val()) * 60;
-  pace += parseInt($("#paceSec").val());
+  var pace = parseInt($("#paceMin").val(),10) * 60;
+  pace += parseInt($("#paceSec").val(),10);
   return pace;
 }
 
 function clampTime(input) {
-  var time = parseInt(input.val());
-  if (time>59)
+  var time = parseInt(input.val(),10);
+  if (time>59) {
     input.val(59);
-  else
-  if (time<10)
+  } else
+  if (time<10) {
     input.val("0"+time);
+  }
   else
-  if (time<0)
+  if (time<0) {
     input.val("00");
+  }
+}
+
+function updatePace() {
+  //console.log("updatePace");
+  undefinedPace = true;
+  var time = getTime();
+  var distance = parseInt($("#distance").val(),10);
+  var pace = time*1000/distance;
+  var paceMin = Math.floor(pace/60);
+  var paceSec =  Math.round(pace-paceMin*60);
+  if (paceSec.length<1) {
+    paceSec = "0"+paceSec;
+  }
+  $("#paceMin").val( paceMin );
+  $("#paceSec").val(paceSec);
+}
+
+function updateTime() {
+  //console.log("updateTime");
+  undefinedPace = false;
+  var pace = getPace();
+  var distance = parseInt($("#distance").val(), 10);
+  var time = distance*pace/1000;
+  var timeHour = Math.floor(time/3600);
+  time -= timeHour*3600;
+  
+  var timeMin = Math.floor(time/60);
+  if (timeMin<10) {
+    timeMin = "0"+timeMin;
+  }
+  
+  time -= timeMin*60;
+  time = Math.round(time);
+  if (time<10) {
+    time = "0"+time;
+  }
+  
+  $("#timeHour").val( timeHour===0 ? "": timeHour );
+  $("#timeMin").val( timeMin );
+  $("#timeSec").val( time );
 }
 
 function onTimeUpdate() {
@@ -56,36 +92,15 @@ function updateDistance() {
   }
 }
 
-function updatePace() {
-  //console.log("updatePace");
-  undefinedPace = true;
-  var time = getTime();
-  var distance = parseInt($("#distance").val());
-  var pace = time*1000/distance;
-  var paceMin = Math.floor(pace/60);
-  var paceSec =  Math.round(pace-paceMin*60);
-  if (paceSec.length<1) paceSec = "0"+paceSec;
-  $("#paceMin").val( paceMin );
-  $("#paceSec").val(paceSec);
-}
+$(document).ready(function() {
+  $("#distance").change(updateDistance);
 
-function updateTime() {
-  //console.log("updateTime");
-  undefinedPace = false;
-  var pace = getPace();
-  var distance = parseInt($("#distance").val());
-  var time = distance*pace/1000;
-  var timeHour = Math.floor(time/3600);
-  time -= timeHour*3600;
-  
-  var timeMin = Math.floor(time/60);
-  if (timeMin<10) timeMin = "0"+timeMin;
-  
-  time -= timeMin*60;
-  time = Math.round(time);
-  if (time<10) time = "0"+time;
-  
-  $("#timeHour").val( timeHour==0 ? "": timeHour );
-  $("#timeMin").val( timeMin );
-  $("#timeSec").val( time );
-}
+  $("#timeHour").change(onTimeUpdate);
+  $("#timeMin").change(onTimeUpdate);
+  $("#timeSec").change(onTimeUpdate);
+
+  $("#paceMin").change(onPaceUpdate);
+  $("#paceSec").change(onPaceUpdate);
+});
+
+})();
